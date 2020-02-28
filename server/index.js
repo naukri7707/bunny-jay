82e3bf74d5deb163cb42cd2f05e5fde2412b807d
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-02-29 05:01:12 +0800

    新增 express-session & body-parser

--- server/index.js
+++ server/index.js
@@ -1,15 +1,27 @@
 // global import
 require("./unitity");
 // import
-const express = require("express"); // 伺服器工具
+const express = require("express"); // 伺服器
+const session = require("express-session"); // session
+const bodyParser = require("body-parser"); // 請求解析
 require("./database/connect.js"); // 連結至資料庫
 const { port, dist } = config.app; // 設定檔
 const routers = require("./router"); // 路由
 /** 連線物件 */
 const app = express();
+const SECRET_KEY = "secret";
+const EXPIRES = 14 * 24 * 60 * 60 * 1000;
 
 // initial
 app.use(express.static(dist));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(
+  session({
+    secret: SECRET_KEY,
+    cookie: { maxAge: EXPIRES }
+  })
+);
 // routers
 routers.forEach(it => {
   app.use(it.path, it.router);
