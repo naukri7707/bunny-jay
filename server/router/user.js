7af79088b6066a9d7fd107e8c2c17de1e2f7d4fc
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-01 03:10:09 +0800

    新增 後端 login & sign-up (不完整)

--- server/router/user.js
+++ server/router/user.js
@@ -4,10 +4,59 @@
 const path = "/user";
 const router = require("express").Router();
 
+const { user } = include("@/database");
+
+// 當前用戶序列號
+let uid = 0;
+
+// 初始化 uid
+user
+  .find({})
+  .sort({ uid: -1 })
+  .limit(1)
+  .exec((err, doc) => {
+    if (err || !doc) {
+      console.log("User ID 初始化失敗");
+    } else {
+      uid = doc[0]._id + 1;
+    }
+  });
+
+// 用戶登入
 router.post("/login", async (req, res) => {
-  // let { username, password } = req.body;
-  // TODO 搜索用戶登入
-  res.json({});
+  const { username, password } = req.body;
+  const doc = await user.findOne({ username });
+  if (doc) {
+    if (password === doc.password) {
+      req.session.regenerate(err => {
+        if (err) {
+          res.status(500).send("Session Error");
+        } else {
+          req.session.userinfo = doc;
+          res.status(200).send(doc.nickname);
+        }
+      });
+    } else {
+      res.status(401).send("密碼錯誤");
+    }
+  } else {
+    res.status(401).send("無此帳號");
+  }
+});
+
+// 用戶註冊
+router.post("/sign-up", async (req, res) => {
+  const data = req.body;
+  // TODO 正則驗證
+  data._id = uid;
+  user.create(data, err => {
+    if (err) {
+      res.json({ code: 1, msg: err });
+    } else {
+      uid++;
+      res.json({ code: 0, msg: "成功" });
+    }
+  });
 });
 
 module.exports = {
