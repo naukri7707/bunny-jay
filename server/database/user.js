82d8b79e319f3676fe8acf3a3cd6795ec6db9c2c
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-01 03:09:31 +0800

    新增 user 資料表

---
+++ server/database/user.js
@@ -0,0 +1,29 @@
+/**
+ * 用戶資料表
+ */
+const { Schema } = require("mongoose");
+const Model = require("mongoose").model;
+
+/** 資料架構 */
+const User = new Schema(
+  {
+    // UID
+    _id: Number,
+    // 帳號
+    username: String,
+    // 密碼
+    password: String,
+    // 暱稱
+    nickname: String,
+    // 電子信箱
+    email: String
+    // ...
+  },
+  {
+    // 不儲存版本號
+    versionKey: false
+  }
+);
+
+// 匯出資料模型 (資料表)
+module.exports = new Model("user", User);
