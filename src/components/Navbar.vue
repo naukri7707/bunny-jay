86b86a1e0ab5033a120434de080ece97fe07d860
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-01 16:44:27 +0800

    新增 登出功能

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,14 +1,14 @@
 <template>
-  <b-navbar type="dark" variant="dark">
+  <b-navbar type="dark" variant="dark" sticky>
     <b-navbar-nav class="ml-auto">
       <b-nav-item-dropdown v-if="isLogin" right>
         <template v-slot:button-content>
           <em>{{ nickname }}</em>
         </template>
         <b-dropdown-item to="#">個人資料</b-dropdown-item>
-        <b-dropdown-item to="#">登出</b-dropdown-item>
+        <b-dropdown-item @click="logout()">登出</b-dropdown-item>
       </b-nav-item-dropdown>
-      <b-nav-item v-else to="/login">登入</b-nav-item>
+      <b-nav-item v-else to="/user/login">登入</b-nav-item>
     </b-navbar-nav>
   </b-navbar>
 </template>
@@ -22,6 +22,21 @@ export default {
     },
     nickname() {
       return this.$store.state.user.nickname;
+    }
+  },
+  methods: {
+    logout() {
+      // TODO toast message
+      this.$store.dispatch("logout").then(
+        ({ data }) => {
+          alert(data);
+          this.$router.push("/user/login");
+        },
+        ({ status, data }) => {
+          alert(`Error ${status}\n\n ${data}`);
+          this.$router.go(0);
+        }
+      );
     }
   }
 };
