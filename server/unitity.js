2791da397e6f965505e2c4d5ebc3bfdabf3d27c3
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-03 01:57:19 +0800

    新增 異步初始化模組

--- server/unitity.js
+++ server/unitity.js
@@ -1,12 +1,34 @@
 /**
  * 通用變數
  */
+
+/**
+ * 使用基於`@`路徑的相對位置引入模組
+ * @param {string} id 相對路徑
+ */
 global.include = id => {
   if (id[0] === "@") {
     return require(id.replace("@", __dirname));
   } else {
     return require(id);
   }
+};
+
+/**
+ * 異步初始化模組
+ * @param {...function} func 欲用來初始化的方法
+ */
+global.init = (...func) => {
+  function wrapper(main, wrap) {
+    return () => {
+      main(wrap);
+    };
+  }
+  func.push(() => {});
+  for (let i = func.length - 2; i >= 0; i--) {
+    func[i] = wrapper(func[i], func[i + 1]);
+  }
+  func[0]();
 };
 
 global.config = require("./config.js");
