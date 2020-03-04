3b905b4eff8e3a86e0343244601c383fbedced7e
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-04 16:45:26 +0800

    新增 產品資訊相關後端

--- server/router/product.js
+++ server/router/product.js
@@ -4,7 +4,7 @@
 const path = "/product";
 const router = require("express").Router();
 
-const { products } = include("@/database");
+const { productInfos, products } = include("@/database");
 
 // 下一個新產品序列號
 let pid = 0;
@@ -24,6 +24,42 @@ init(next => {
   );
 });
 
+// 取得產品資訊
+router.get("/info", async (req, res) => {
+  let doc = await productInfos.find({});
+  if (doc) {
+    res.status(200).json(doc);
+  } else {
+    res.status(500).send("找不到資料");
+  }
+});
+
+// 新增產品資訊
+router.post("/addInfo", (req, res) => {
+  let data = req.body;
+  let pid = 0;
+  productInfos.aggregate(
+    [{ $group: { _id: null, max: { $max: "$_id" } } }],
+    (err, res) => {
+      if (res.length === 0) {
+        pid = 1;
+      } else {
+        pid = res[0].max + 1;
+      }
+    }
+  );
+  data._id = pid;
+  productInfos.create(data, err => {
+    if (err) {
+      res.status(500).send("新增失敗");
+    } else {
+      pid++;
+      res.status(200).send("新增成功");
+    }
+  });
+});
+
+// 更新產品狀態
 router.get("/update", async (req, res) => {
   let doc = await products.find({ product: req.query.product });
   if (doc) {
@@ -33,6 +69,7 @@ router.get("/update", async (req, res) => {
   }
 });
 
+// 新增隨機資料
 router.get("/addRandom", (req, res) => {
   let data = req.query;
   data._id = pid;
