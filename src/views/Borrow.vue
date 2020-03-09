c56d4908a758dd3432e2e00d6bc4bba6e3765277
Naukri <50033668+naukri7707@users.noreply.github.com>
2020-03-09 22:31:49 +0800

    新增 回收按鈕 (僅在有權限的用戶上顯示)

--- src/views/Borrow.vue
+++ src/views/Borrow.vue
@@ -2,17 +2,39 @@
   <b-container id="product">
     <h1>{{ product.name }}</h1>
     <div>類型：{{ productInfo.zhName }}</div>
-    <div v-if="user.username">
+    <div v-if="user._id">
       使用者：{{ user.nickname }} ({{ user.username }})
+      <div>
+        租借時間：{{ new Date(product.borrowTime).format("yyyy/MM/dd") }}
+      </div>
+      <div>到期時間：{{ deadDate }}</div>
     </div>
     <div v-else>
-      使用者：無
+      使用者：目前沒有人租用
+      <div>
+        可租借時間：{{ productInfo.day ? `${productInfo.day}天` : "當天還" }}
+      </div>
+      <div>
+        到期時間：{{
+          new Date(Date.now() + productInfo.day * Date.timeOfDay).format(
+            "yyyy/MM/dd"
+          )
+        }}
+      </div>
     </div>
-    <div>租借時間：{{ new Date(product.borrowTime).format("yyyy/MM/dd") }}</div>
-    <div>到期時間：{{ deadDate }}</div>
+
     <br />
-    <b-button variant="info" @click="borrow">
-      Bunny Jay 這個
+
+    <b-button variant="primary" @click="borrow()">
+      Bunny Jay
+    </b-button>
+    <br />
+    <b-button
+      v-if="$store.state.user.usergroup >= 90"
+      variant="secondary"
+      @click="retrieve()"
+    >
+      回收
     </b-button>
   </b-container>
 </template>
@@ -87,11 +109,29 @@ export default {
             this.toast(msg, { title: "租借成功", variant: "success" });
           } else {
             let msg =
-              user.uid === this.$store.state.user.uid
+              user._id === this.$store.state.user.uid
                 ? "你已經借到拉！"
                 : `「${product.name}」已被 ${user.nickname}(${user.username}) 租用`;
             this.toast(msg, { title: "無法預借", variant: "warning" });
           }
+        },
+        ({ status, data }) => {
+          this.toast(data, {
+            title: `Error ${status}`,
+            variant: "danger"
+          });
+          if (status === 401) {
+            this.$router.push("/user/login");
+          }
+        }
+      );
+    },
+    retrieve() {
+      this.$store.dispatch("product/retrieve", this.$route.query.pid).then(
+        ({ data }) => {
+          this.product = data;
+          this.user = { _id: 0, username: "", nickname: "" };
+          this.toast("回收完成", { title: "回收成功", variant: "success" });
         },
         ({ status, data }) => {
           this.toast(data, {
